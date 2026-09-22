72dfbf5acf2f70eb753cd59c3bbe178991f5e3d3
tamabc906-coder <tamabc906@gmail.com>
2026-09-22 12:09:36 +0700

    G5: VAPID public key vào docs/config.js

--- docs/config.js
+++ docs/config.js
@@ -2,6 +2,6 @@
 // (sinh bằng venv\Scripts\python -m job.gen_vapid). Để trống = nút "Bật thông báo" bị khoá.
 // WORKER_URL trống = điện thoại hiện đoạn mã đăng ký để dán vào Secret PUSH_SUBS_FALLBACK.
 window.WR_CONFIG = {
-  VAPID_PUBLIC: "",
+  VAPID_PUBLIC: "BCBHH0wM3bwDbJqiJfRygr9lHmVeGUUWf8iRXm4OfCDVTUpa8uRJz05k2hONniYzQXE8HxyUnsUkwY2DDyUYWGE",
   WORKER_URL: "",
 };
